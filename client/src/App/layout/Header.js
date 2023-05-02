a918d414e28532cbce610f186c74bf22b85a7c55
sajjadjln <sajjadjlm1999@gmail.com>
2023-05-02 16:54:20 +0330

    adding margin between login,button

--- client/src/App/layout/Header.js
+++ client/src/App/layout/Header.js
@@ -13,20 +13,22 @@ export default function Header(props) {
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" sx={{mb:4}}>
-        <Toolbar>
+      <AppBar position="static" sx={{mb: 4}}>
+        <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Nanvae
           </Typography>
-          <ToggleButtonGroup value={darkMode} exclusive onChange={handleThemeChange}>
-            <ToggleButton value={false}>
-              <Brightness7 />
-            </ToggleButton>
-            <ToggleButton value={true}>
-              <Brightness4 />
-            </ToggleButton>
-          </ToggleButtonGroup>
-          <Button color="inherit">Login</Button>
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <ToggleButtonGroup value={darkMode} exclusive onChange={handleThemeChange}>
+              <ToggleButton value={false}>
+                <Brightness7 />
+              </ToggleButton>
+              <ToggleButton value={true}>
+                <Brightness4 />
+              </ToggleButton>
+            </ToggleButtonGroup>
+            <Button color="inherit" sx={{ marginLeft: 2 }}>Login</Button>
+          </Box>
         </Toolbar>
       </AppBar>
     </Box>
