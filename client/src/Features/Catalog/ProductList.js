ff4b77372b93285b5a1f04bc437ff36e5019fd74
sajjadjln <sajjadjlm1999@gmail.com>
2023-05-02 16:54:36 +0330

    using styled()

--- client/src/Features/Catalog/ProductList.js
+++ client/src/Features/Catalog/ProductList.js
@@ -1,14 +1,28 @@
-import ProductCard from "./ProductCard"
-import { Grid } from '@mui/material';
+import ProductCard from "./ProductCard";
+import Grid from '@mui/material/Grid';
+import styled from '@emotion/styled';
+
+
+
+const useStyles = styled((theme) => ({
+  root: {
+    flexGrow: 1,
+    margin: theme.spacing(2),
+  },
+}));
 
 export default function ProductList(props) {
+  const classes = useStyles();
+
   return (
-    <Grid container spacing={4}>
-      {props.products.map((product) => (
-        <Grid item xs={3} key={product.id}>
-          <ProductCard  product={product} />
-        </Grid>
-      ))}
-    </Grid>
-  )
+    <div className={classes.root}>
+      <Grid container spacing={4}>
+        {props.products.map((product) => (
+          <Grid item xs={12} sm={6} md={4} lg={3} key={product.id}>
+            <ProductCard product={product} />
+          </Grid>
+        ))}
+      </Grid>
+    </div>
+  );
 }
