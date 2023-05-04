0fb38dd420fcd2987b69055c463a4c76908bf879
sajjadjln <sajjadjlm1999@gmail.com>
2023-05-05 02:27:03 +0330

    extending Entity

---
+++ Infrastructure/Data/Config/ProductConfiguration.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Data.Config
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.Property(p => p.Id).IsRequired();
+            builder.Property(p => p.Name).IsRequired().HasMaxLength(100);
+            builder.Property(p => p.Description).IsRequired();
+            builder.Property(p => p.Price).IsRequired().HasColumnType("decimal(18,2)"); // 18 digits, 2 decimal places
+            
+        }
+    }
+}
