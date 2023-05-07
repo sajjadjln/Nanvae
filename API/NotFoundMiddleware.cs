8e001f41d48e5ffe7b3dbbdc2886b76ce2f8f581
sajjadjln <sajjadjlm1999@gmail.com>
2023-05-07 15:28:38 +0330

    404 middleware

---
+++ API/NotFoundMiddleware.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+namespace API
+{
+public class NotFoundMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public NotFoundMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        await _next(context);
+
+        if (context.Response.StatusCode == 404)
+        {
+            context.Response.ContentType = "application/json";
+            var response = new { message = $"The resource you requested could not be found. status code : {context.Response.StatusCode}" };
+            var json = JsonConvert.SerializeObject(response);
+            await context.Response.WriteAsync(json);
+        }
+    }
+}
+
+}
