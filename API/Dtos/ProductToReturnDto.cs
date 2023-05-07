0708f4f45ff102cdace1b4a7c0c0644db6c59766
sajjadjln <sajjadjlm1999@gmail.com>
2023-05-07 15:29:06 +0330

    dto to shape data

---
+++ API/Dtos/ProductToReturnDto.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities;
+
+namespace API.Dtos
+{
+    public class ProductToReturnDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ProductType { get; set; }
+        public string ProductBrand { get; set; }
+        public Decimal Price { get; set; }
+        public string PictureUrl { get; set; }
+    }
+}
