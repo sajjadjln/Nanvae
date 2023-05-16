cb53e29db13898e075df770ff1812a8a45130729
sajjadjln <sajjadjlm1999@gmail.com>
2023-05-16 13:02:30 +0330

    few changes on response

--- API/Middleware/ExceptionMiddleware.cs
+++ API/Middleware/ExceptionMiddleware.cs
@@ -1,8 +1,5 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Security.Cryptography.X509Certificates;
 using System.Text.Json;
 using System.Threading.Tasks;
 using API.Errors;
@@ -29,15 +26,15 @@ namespace API.Middleware
             {
                 await _next(context);
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 _logger.LogError(ex, ex.Message);
                 context.Response.ContentType = "application/json";
-                context.Response.StatusCode = (int) HttpStatusCode.InternalServerError;
-                var response = _env.IsDevelopment() ? new ApiExceptions((int)HttpStatusCode.InternalServerError,
+                context.Response.StatusCode = 500; //(int) HttpStatusCode.InternalServerError;
+                var Detail = _env.IsDevelopment() ? new ApiExceptions((int)HttpStatusCode.InternalServerError,
                     ex.Message, ex.StackTrace.ToString()) : new ApiExceptions((int)HttpStatusCode.InternalServerError);
-                var options = new JsonSerializerOptions{PropertyNamingPolicy = JsonNamingPolicy.CamelCase};
-                var json = JsonSerializer.Serialize(response, options);
+                var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+                var json = JsonSerializer.Serialize(Detail, options);
                 await context.Response.WriteAsync(json);
             }
         }
