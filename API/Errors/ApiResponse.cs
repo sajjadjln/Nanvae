8e7ec648092db46c270fcdf1fdf67ca7ef327239
sajjadjln <sajjadjlm1999@gmail.com>
2023-05-16 13:01:23 +0330

    adding 405

--- API/Errors/ApiResponse.cs
+++ API/Errors/ApiResponse.cs
@@ -22,7 +22,8 @@ namespace API.Errors
                 400 => "A bad request, you have made",
                 401 => "Authorized, you are not",
                 404 => "Resource found, it was not",
-                500 => "Errors are the path to the dark side. Errors lead to anger. Anger leads to hate. Hate leads to career change",
+                405 => "one or more validation errors occurred",
+                500 => "server error",
                 _ => null // if non of this match then we return a default case
             };
         }
