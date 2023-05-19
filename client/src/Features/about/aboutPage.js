30ba9cf688afe7a89a12a963853eb675232475f7
sajjadjln <sajjadjlm1999@gmail.com>
2023-05-19 11:03:19 +0330

    extending the error testing

--- client/src/Features/about/aboutPage.js
+++ client/src/Features/about/aboutPage.js
@@ -1,17 +1,41 @@
 import Typography from '@mui/material/Typography';
-import { Button, ButtonGroup, Container } from '@mui/material';
+import { Alert, AlertTitle, Button, ButtonGroup, Container, ListItemText } from '@mui/material';
 import agent from '../../App/api/agent';
+import { useState } from 'react';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+
 export default function AboutPage() {
-    return (
-        <Container>
-            <Typography gutterBottom variant='h2'>Errors for testing purposes</Typography>
-            <ButtonGroup>
-                <Button variant='contained' onClick={()=>agent.TestErrors.get400Error().catch(error=> console.log(error))}>Test 400</Button>
-                <Button variant='contained' onClick={() => agent.TestErrors.get401Error().catch(error => console.log(error))}>Test 401</Button>
-                <Button variant='contained' onClick={()=>agent.TestErrors.get404Error().catch(error=> console.log(error))}>Test 404</Button>
-                <Button variant='contained' onClick={()=>agent.TestErrors.get500Error().catch(error=> console.log(error))}>Test 500</Button>
-                <Button variant='contained' onClick={()=>agent.TestErrors.get400ValidationError().catch(error=> console.log(error))}>Test Validation Error</Button>
-            </ButtonGroup>
-        </Container>
-    );
+  const [validationErrors, setValidationErrors] = useState([]);
+
+  function getValidationError() {
+    agent.TestErrors.getValidationError()
+      .then(() => console.log('you should not see this'))
+      .catch((error) => setValidationErrors(error));
+  }
+
+  return (
+    <Container>
+      <Typography gutterBottom variant='h2'>Errors for testing purposes</Typography>
+      <ButtonGroup>
+        <Button variant='contained' onClick={() => agent.TestErrors.get400Error().catch((error) => console.log(error))}>Test 400</Button>
+        <Button variant='contained' onClick={() => agent.TestErrors.get401Error().catch((error) => console.log(error))}>Test 401</Button>
+        <Button variant='contained' onClick={() => agent.TestErrors.get404Error().catch((error) => console.log(error))}>Test 404</Button>
+        <Button variant='contained' onClick={() => agent.TestErrors.get500Error().catch((error) => console.log(error))}>Test 500</Button>
+        <Button variant='contained' onClick={getValidationError}>Test Validation Error</Button>
+      </ButtonGroup>
+      {validationErrors.length > 0 && ( // Modified condition to check for validationErrors length
+        <Alert severity='error'>
+          <AlertTitle>Validation errors</AlertTitle>
+          <List>
+            {validationErrors.map((error, index) => (
+              <ListItem key={index}>
+                <ListItemText>{error}</ListItemText>
+              </ListItem>
+            ))}
+          </List>
+        </Alert>
+      )}
+    </Container>
+  );
 }
