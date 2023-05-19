170ba8b4c71da61f581f1fb6e6099e9ff3595260
sajjadjln <sajjadjlm1999@gmail.com>
2023-05-19 11:01:06 +0330

    extending error handling

--- client/src/App/api/agent.js
+++ client/src/App/api/agent.js
@@ -1,18 +1,30 @@
-import axios  from 'axios';
+import axios from 'axios';
 import { toast } from 'react-toastify';
+
+
 axios.defaults.baseURL = 'https://localhost:5002/api/';
 
 const responseBody = (response) => response.data;
+
 axios.interceptors.response.use(
   (response) => {
-    return response;
+    return new Promise((resolve) => setTimeout(() => resolve(response), 500));
   },
   (error) => {
     const { data, status } = error.response;
     console.log(data); // Log the data object to inspect its properties
     switch (status) {
       case 400:
-        toast.error(data.message);
+        if (data.errors) {
+          const modalStateErrors = [];
+          for (const key in data.errors) {
+            if (data.errors[key]) {
+              modalStateErrors.push(data.errors[key]);
+            }
+          }
+          throw modalStateErrors.flat();
+        }
+        toast.error(data.title);
         break;
       case 401:
         toast.error(data.message);
@@ -21,7 +33,7 @@ axios.interceptors.response.use(
         toast.error(data.message);
         break;
       case 500:
-        toast.error("server error");
+        window.location.href = '/server-error'; // Redirect to server error page
         break;
       default:
         toast.error(data.message);
@@ -32,27 +44,28 @@ axios.interceptors.response.use(
 );
 
 const requests = {
-    get: (url) => axios.get(url).then(responseBody),
-    post: (url, body) => axios.post(url, body).then(responseBody),
-    put: (url, body) => axios.put(url, body).then(responseBody),
-    del: (url) => axios.delete(url).then(responseBody)
-}
+  get: (url) => axios.get(url).then(responseBody),
+  post: (url, body) => axios.post(url, body).then(responseBody),
+  put: (url, body) => axios.put(url, body).then(responseBody),
+  del: (url) => axios.delete(url).then(responseBody),
+};
 
 const catalog = {
-    list: () => requests.get('/Product'),
-    details: (id) => requests.get(`/Product/${id}`)
-}
-
+  list: () => requests.get('/Product'),
+  details: (id) => requests.get(`/Product/${id}`),
+};
 
 const TestErrors = {
-    get404Error: () => requests.get('/buggy/not-found'),
-    get400Error: () => requests.get('/buggy/bad-request'),
-    get500Error: () => requests.get('/buggy/servererror'),
-    get401Error: () => requests.get('/buggy/auth'),
-    get400ValidationError: () => requests.post('/buggy/validation-error')
-}
+  get404Error: () => requests.get('/buggy/notfound'),
+  get400Error: () => requests.get('/buggy/badrequest'),
+  get500Error: () => requests.get('/buggy/servererror'),
+  get401Error: () => requests.get('/buggy/auth'),
+  getValidationError: () => requests.post('/buggy/validation-error'),
+};
+
 const agent = {
-    catalog,
-    TestErrors
-}
+  catalog,
+  TestErrors,
+};
+
 export default agent;
