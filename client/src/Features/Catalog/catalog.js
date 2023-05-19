d9eb2ba67c11f730b069fddbeb994f1393f42a28
sajjadjln <sajjadjlm1999@gmail.com>
2023-05-19 11:03:37 +0330

    adding loading

--- client/src/Features/Catalog/catalog.js
+++ client/src/Features/Catalog/catalog.js
@@ -2,20 +2,25 @@ import React from "react";
 import ProductList from "./ProductList";
 import { useState, useEffect } from "react";
 import agent from "../../App/api/agent";
+import LoadingComponent from "../../App/layout/LoadingComponent";
 
 export default function Catalog() {
   const [products, setProducts] = useState([]
-    );
-    useEffect(() => {
-      agent.catalog.list().then((response) => {
-        setProducts(response);
-      });
-      // fetch("https://localhost:5002/api/Product")
-      //   .then(response => response.json())
-      //   .then(data => setProducts(data))
-      //promise , callback function,
-      // dependency of empty array(only getting called once other wise it will keep calling products)
-    }, []);
+  );
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    agent.catalog.list().then((response) => {
+      setProducts(response);
+    }).catch(error => console.log(error)).finally(() => setLoading(false));
+    // fetch("https://localhost:5002/api/Product")
+    //   .then(response => response.json())
+    //   .then(data => setProducts(data))
+    //promise , callback function,
+    // dependency of empty array(only getting called once other wise it will keep calling products)
+  }, []);
+  if (loading) {
+    return (<LoadingComponent message="Loading products"/>)
+  }
   return (
     <>
       <ProductList products={products} />
